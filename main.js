f062efa6299efe3e1842171d23db6b54ccfc8168
adityamathur430 <83180834+adityamathur430@users.noreply.github.com>
2023-01-08 11:23:22 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,56 @@
+objects = [];
+status = "";
+input ="";
+
+function setup() {
+    canvas = createCanvas(480,380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(480,380);
+    video.hide();
+}
+
+function start() {
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+    input = document.getElementById("Object_text_input").value;
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!");
+    status = true;
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
+
+function draw() {
+    image(video, 0, 0, 480, 380);
+    if(status != "")
+    {
+        objectDetector.detect(video, gotResult);
+        for (i = 0; i< objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status : Objects Detected";
+
+            fill("white");
+            percent = floor(objects[i].confidence *100);
+            text(objects[i].label + " "+percent+"%", objects[i].x +15, objects[i].y+15);
+            noFill();
+            stroke("white");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height); 
+            if(objects[i].label == input)
+            {document.getElementById("object_found").innerHTML = input+" Found";
+            }
+            else
+            {
+                document.getElementById("object_found").innerHTML = input+" Not Found";
+            }
+        }
+    }
+}
+
